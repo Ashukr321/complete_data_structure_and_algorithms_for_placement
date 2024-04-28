ee63fba92aff6fda0148ef87c580a9cd9b463643
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-04-28 18:12:33 +0530

    first commit

---
+++ sorting/intro.cpp
@@ -0,0 +1,12 @@
+//preprocessor
+#include<iostream>
+
+using namespace std;
+
+
+//main start here ...
+int main(){
+  cout<<"welcome to complete data structure and algorithms for placement "<<endl;
+  
+  return 0;
+}
