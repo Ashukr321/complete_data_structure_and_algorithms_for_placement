845fc96d6557677ef09da9dc8afd6253cbae258b
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-05-03 18:55:35 +0530

    binary search  implementation using array  iterative

---
+++ Array_implementation_in_cpp/recurence_relation.cpp
@@ -0,0 +1,30 @@
+//preprocessor
+#include<iostream>
+using namespace std;
+
+
+//main start here ...
+int main(){
+  //  recurrence relation in the algorithm 
+  /*
+   recurrence relation are commonly use to define the time complexity of the algorithm 
+   ⭐⭐ time and space complexity 
+   // time and space complexity is the key aspect of the algorithm 
+   time complexity is expressed in terms of the asymptotic notations 
+
+   space complexity is is the refers to  the amount of the space is required to execute the algorithm 
+    both time and space complexity is required for evaluating the algorithms 
+
+  recreance relation -> when algorithm contains recursive call func. call it self then the relation is defined by the help of the recreance relation 
+   
+   to solve the recreance relation we have 3  
+   srm
+    substituion methods 
+    master methods 
+    recursive tree methods to solve the time and space complexity 
+
+    
+
+  */
+  return 0;
+}
