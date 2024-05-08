87ca4c1a8428a9dcb9059e42d83aebb44c472684
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-05-08 12:19:03 +0530

    start implementing the string in the cpp

---
+++ string_implementation_in_cpp/intro.cpp
@@ -0,0 +1,21 @@
+//preprocessor
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+
+//main start here ...
+int main(){
+  string name = "Ashutosh kumar";
+  reverse(name.begin(),name.end());
+  cout<<name.length()<<endl;
+
+  for ( auto it :name)
+  {
+    cout<<name<<endl;
+  }
+  
+    cout<<name<<endl;
+  return 0;
+}
