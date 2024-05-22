59ee34d89f22724791167c0d15bbf7c3c312b5fd
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-05-22 11:44:10 +0530

    intro to vector

---
+++ complete_stl_cpp/vector/intro.cpp
@@ -0,0 +1,48 @@
+// preprocessor
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void printData(vector<int> v1)
+{
+  for (int i = 0; i < v1.size(); i++)
+  {
+    cout << v1[i] << " ";
+  }
+}
+// main start here ...
+int main()
+{
+  // vector in the cpp
+  // vector  is the dynamic  arrray
+  // vector<int> v1;// this is the empty vector with data type int
+
+  vector<int> v1(5, 10); // size is 5 and element is assign 10 ;
+  vector<int> v2 = {
+      1,
+      2,
+      3,
+      4,
+  }; // create and initialize the vector
+  cout << v2[2] << endl;
+  cout << v1[0] << endl;
+
+  v2.push_back(12);
+  printData(v2);
+
+  v2.push_back(34);
+  cout<<"\n After push back the elements"<<endl;
+  printData(v2);
+
+  v2.pop_back();
+  cout<<endl;
+  cout<<v2.size()<<endl;
+  cout<<endl;
+  cout<<v2.at(2)<<endl;
+  cout<<v2.front()<<endl;
+  cout<<v2.back()<<endl;
+  v2.clear();
+  cout<<v2.capacity()<<endl;
+  return 0;
+}
