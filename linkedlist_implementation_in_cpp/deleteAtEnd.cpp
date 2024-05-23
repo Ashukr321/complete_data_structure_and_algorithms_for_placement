fbb5af70ffbec3d875a1b30140f68170281ad3a7
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-05-23 07:52:51 +0530

    delete node at begin

---
+++ linkedlist_implementation_in_cpp/deleteAtEnd.cpp
@@ -0,0 +1,57 @@
+//preprocessor
+#include<iostream>
+using namespace std;
+
+class Node {
+  public:
+  int data;
+  Node* next;
+  Node(int data){
+    this->data =data;
+    this->next = NULL;
+  }
+};
+
+void  displayNode(Node * head){
+  Node * temp= head;
+  while (temp !=NULL)
+  {
+     cout<<temp->data<<"->";
+     temp = temp ->next;
+  }
+  cout<<"NULL"<<endl;
+}
+
+void insertNodeAtBegin( Node * & head ,int data){
+  Node * newNode = new Node(data);
+  if(head ==NULL){
+    head = newNode;
+    return;
+  }
+  
+  newNode->next= head;
+  head = newNode;
+}
+
+void deleteAtBegin(Node *  &head ){
+  if(head == NULL){
+    cout<<"linked list is already null! "<<endl;
+    return;
+  }
+  Node * temp = head;
+  head = head->next;
+   delete temp;
+
+
+}
+//main start here ...
+int main(){
+  Node * head = NULL;
+  insertNodeAtBegin(head,12);
+  insertNodeAtBegin(head,2);
+  insertNodeAtBegin(head,1);
+  insertNodeAtBegin(head,129);
+  deleteAtBegin(head);
+  displayNode(head);
+  return 0;
+}
--- linkedlist_implementation_in_cpp/length_of_linkedlist.cpp
+++ linkedlist_implementation_in_cpp/length_of_linkedlist.cpp
@@ -4,73 +4,71 @@ using namespace std;
 class Node
 {
 public:
-    int data;
-    Node *next;
+  int data;
+  Node *next;
 
-    // constructor
-    Node(int data)
-    {
-        this->data = data;
-        this->next = NULL;
-    }
-
-   
+  // constructor
+  Node(int data)
+  {
+    this->data = data;
+    this->next = NULL;
+  }
 };
 
 void displayNode(Node *head)
 {
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->data << " -> ";
-        temp = temp->next;
-    }
-    cout << "NULL" << endl;
+  Node *temp = head;
+  while (temp != NULL)
+  {
+    cout << temp->data << " -> ";
+    temp = temp->next;
+  }
+  cout << "NULL" << endl;
 }
 
 void insertNodeBegin(Node *&head, int data)
 {
-    Node *newNode = new Node(data);
-    if (head == NULL)
-    {
-        head = newNode;
-        return;
-    }
-    newNode->next = head;
+  Node *newNode = new Node(data);
+  if (head == NULL)
+  {
     head = newNode;
+    return;
+  }
+  newNode->next = head;
+  head = newNode;
 }
 
 int lengthOfNode(Node *head)
 {
-    int count = 0;
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        count++;
-        temp = temp->next;
-    }
-    return count;
+  int count = 0;
+  Node *temp = head;
+  while (temp != NULL)
+  {
+    count++;
+    temp = temp->next;
+  }
+  return count;
 }
 
 // main start here ...
 int main()
 {
-    Node *head = NULL;
-    insertNodeBegin(head, 1);
-    insertNodeBegin(head, 10);
-    insertNodeBegin(head, 20);
-    insertNodeBegin(head, 5);
-    displayNode(head);
+  Node *head = NULL;
+  insertNodeBegin(head, 1);
+  insertNodeBegin(head, 10);
+  insertNodeBegin(head, 20);
+  insertNodeBegin(head, 5);
+  displayNode(head);
 
-    cout << "Length of the linked list: " << lengthOfNode(head) << endl;
-    
-    // Clean up the linked list to avoid memory leaks
-    while (head != NULL)
-    {
-        Node *temp = head;
-        head = head->next;
-        delete temp;
-    }
-    
-    return 0;
+  cout << "Length of the linked list: " << lengthOfNode(head) << endl;
+
+  // Clean up the linked list to avoid memory leaks
+  while (head != NULL)
+  {
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+  }
+
+  return 0;
 }
