3a74285b8f8a3e20bfc5aa1fbd33807dc09dab2c
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-06-08 16:06:20 +0530

    check the array is sorted or not in v_2.0

---
+++ array_implementation_v_2.0/check_array_is_sorted.cpp
@@ -0,0 +1,50 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+bool isSorted(int arr[], int size)
+{
+  for (int i = 0; i < size - 1; i++)
+  {
+    if (arr[i] > arr[i +1 ])
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+// main start here ...
+int main()
+{
+  int size;
+  cout << "Enter the size of the array : " << endl;
+  cin >> size;
+  int arr[size];
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the element at index: " << i << endl;
+    cin >> n;
+    arr[i] = n;
+  }
+
+  if (isSorted(arr, size))
+  {
+    cout << "Arrays is sorted" << endl;
+    for (int i = 0; i < size; i++)
+    {
+      cout << arr[i] << endl;
+    }
+  }
+  else
+  {
+    cout << "array is not sorted: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+      cout << arr[i] << endl;
+    }
+  }
+
+  return 0;
+}
