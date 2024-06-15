15ea6fe0c69533d4c32a4bc95f11ba79563abcc0
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-06-15 14:48:08 +0530

    delete node at begin

---
+++ linked_list_v2.o/deleteNode_atbegin_in_singly.cpp
@@ -0,0 +1,62 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next = nullptr;
+
+  Node(int data)
+  {
+    this->data = data;
+    this->next = nullptr;
+  }
+};
+
+void displayNode(Node *head)
+{
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    cout << temp->data << " ->";
+    temp = temp->next;
+  }
+}
+
+void insertNodeAtBegin(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  newNode->next = temp;
+  head = newNode;
+}
+
+void deleteNodeAtBegin( Node * &head){
+  if( head ==nullptr){
+    cout<<"node is already empty : "<<endl;
+    return;
+
+  }
+  Node *temp = head;
+  head = temp ->next;
+  delete temp;
+}
+// main start here ...
+int main()
+{
+  Node * head  = nullptr;
+  insertNodeAtBegin(head, 1);
+  insertNodeAtBegin(head, 2);
+  insertNodeAtBegin(head, 3);
+  insertNodeAtBegin(head, 4);
+  deleteNodeAtBegin(head);
+  displayNode(head);
+  return 0;
+}
