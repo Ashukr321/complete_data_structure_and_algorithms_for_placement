a58fa6858b8269fa084080fbadac68884d704d95
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-06-25 22:13:21 +0530

    find the maximum element of the array

---
+++ 30_days_chllanges/day1/_17_find_maximum_of_array.cpp
@@ -0,0 +1,34 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+int findMx(int arr[],int size){
+  int max = INT_MIN;
+  for (int i = 0; i < size; i++)
+  {
+      if(arr[i]>max){
+        max = arr[i];
+      }
+  }
+  
+  return max;
+}
+// main start here ...
+int main()
+{
+  int size;
+  cout << "Enter the size of the array " << endl;
+  cin >> size;
+  int arr[size];
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the element " << i << endl;
+    cin >> n;
+    arr[i] = n;
+  }
+  int maxElement = findMx(arr,size);
+  cout<<"maximum element of the array : "<< maxElement<<endl;
+
+  return 0;
+}
