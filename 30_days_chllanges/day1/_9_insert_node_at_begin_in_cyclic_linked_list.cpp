14ece4a88d0723abaea1235df66b5262d7723295
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-06-25 18:53:33 +0530

    insert the node at the begin of the singly circular linked list

--- 30_days_chllanges/day1/_9_insert_node_at_begin_in_cyclic_linked_list.cpp
+++ 30_days_chllanges/day1/_9_insert_node_at_begin_in_cyclic_linked_list.cpp
@@ -13,19 +13,21 @@ public:
     this->next = nullptr;
   }
 };
-
 void displayNode(Node *head)
 {
-  if (head == nullptr) return;
-  
+  if (head == nullptr)
+  {
+    return;
+  }
   Node *temp = head;
   do
   {
-    cout << temp->data << " -> ";
+    cout << temp->data << "->";
     temp = temp->next;
   } while (temp != head);
-  cout << "HEAD" << endl; // Indicate the end of the circular linked list
+  cout << "head" << endl;
 }
+
 void insertNodeAtBegin(Node *&head, int data)
 {
   Node *newNode = new Node(data);
@@ -35,7 +37,6 @@ void insertNodeAtBegin(Node *&head, int data)
     newNode->next = head;
     return;
   }
-
   Node *temp = head;
   while (temp->next != head)
   {
@@ -45,16 +46,16 @@ void insertNodeAtBegin(Node *&head, int data)
   temp->next = newNode;
   head = newNode;
 }
+
 // main start here ...
 int main()
 {
   Node *head = nullptr;
-
-  insertNodeAtBegin(head, 13);
-  insertNodeAtBegin(head, 12);
-  insertNodeAtBegin(head, 13);
-  insertNodeAtBegin(head, 14);
-  insertNodeAtBegin(head, 15);
+  insertNodeAtBegin(head, 1);
+  insertNodeAtBegin(head, 2);
+  insertNodeAtBegin(head, 3);
+  insertNodeAtBegin(head, 4);
   displayNode(head);
+
   return 0;
 }
