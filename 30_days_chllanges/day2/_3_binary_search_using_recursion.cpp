a85d7302987dc7436430d379c9af31a2d5fddc55
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-06-26 19:35:27 +0530

    print the node using recursion in the singly linked list

---
+++ 30_days_chllanges/day2/_3_binary_search_using_recursion.cpp
@@ -0,0 +1,11 @@
+//preprocessor
+#include<iostream>
+using namespace std;
+
+
+//main start here ...
+int main(){
+  
+  return 0;
+}
+//binary search is work on the sorted array 
---
+++ 30_days_chllanges/day2/_5_print_the_singly_linked_list_using_recrusion.cpp
@@ -0,0 +1,56 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node(int data)
+  {
+    this->data = data;
+    this->next = nullptr;
+  }
+};
+
+void displayNode(Node *head)
+{
+  // base case
+  if (head == nullptr)
+  {
+    cout << "nullptr" << endl;
+    return;
+  }
+  cout << head->data << "->";
+
+  displayNode(head->next);
+}
+
+void insertNodeAtEnd(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  while (temp->next != nullptr)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+}
+
+// main start here ...
+int main()
+{
+  Node *head = nullptr;
+  insertNodeAtEnd(head, 1);
+  insertNodeAtEnd(head, 2);
+  insertNodeAtEnd(head, 3);
+  insertNodeAtEnd(head, 4);
+  displayNode(head);
+  return 0;
+}
