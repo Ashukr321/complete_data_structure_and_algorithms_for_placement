51fcd72f023de1aa1ca53fabbe4e44044bdab989
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-06-28 01:52:08 +0530

    check the string is palindrome or not

--- Recursion/_9_check_string_is_palindrom.cpp
+++ Recursion/_9_check_string_is_palindrom.cpp
@@ -4,33 +4,33 @@ using namespace std;
 
 bool checkPalindrome(string str, int start, int end)
 {
-    if (start >= end)
-    {
-        return true;
-    }
-    if (str[start] != str[end])
-    {
-        return false;
-    }
-    return checkPalindrome(str, start + 1, end - 1);
+  if (start >= end)
+  {
+    return true;
+  }
+  if (str[start] != str[end])
+  {
+    return false;
+  }
+  return checkPalindrome(str, start + 1, end - 1);
 }
 
 // main starts here ...
 int main()
 {
-    string str;
-    cout << "Enter your string:" << endl;
-    cin >> str;
-    cout << "Your string is: " << str << endl;
+  string str;
+  cout << "Enter your string:" << endl;
+  cin >> str;
+  cout << "Your string is: " << str << endl;
 
-    if (checkPalindrome(str, 0, str.length() - 1))
-    {
-        cout << "The string is a palindrome." << endl;
-    }
-    else
-    {
-        cout << "The string is not a palindrome." << endl;
-    }
+  if (checkPalindrome(str, 0, str.length() - 1))
+  {
+    cout << "The string is a palindrome." << endl;
+  }
+  else
+  {
+    cout << "The string is not a palindrome." << endl;
+  }
 
-    return 0;
+  return 0;
 }
