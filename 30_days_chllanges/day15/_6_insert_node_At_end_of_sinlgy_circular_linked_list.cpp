eda898920dfa5d08815530867a52f309dd2b9726
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-07-15 08:14:28 +0530

    insert node at end  of the singly linked list

---
+++ 30_days_chllanges/day15/_6_insert_node_At_end_of_sinlgy_circular_linked_list.cpp
@@ -0,0 +1,64 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node(int data)
+  {
+    this->data = data;
+    this->next = NULL;
+  }
+  static void displayNode(Node *head);
+  static void insertNodeAtEnd(Node *&head, int data);
+};
+void Node::displayNode(Node *head)
+{
+  Node *temp = head;
+  do
+  {
+    cout << temp->data << "->";
+    temp = temp->next;
+  } while (temp != head);
+  cout << "HEAD" << endl;
+}
+void Node::insertNodeAtEnd(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  if (head == nullptr)
+  {
+    head = newNode;
+    newNode->next = head;
+    return;
+  }
+  Node *temp = head;
+  while (temp->next != head)
+  {
+    temp = temp->next;
+  }
+  newNode->next = temp->next;
+  temp->next = newNode;
+}
+
+// main start here ...
+int main()
+{
+  Node *head = nullptr;
+  int size;
+  cout << "Enter the size of the linked list:" << endl;
+  cin >> size;
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the  element at index:" << i << endl;
+    cin >> n;
+    Node::insertNodeAtEnd(head, n);
+  }
+
+  cout << "display All Node:" << endl;
+  Node::displayNode(head);
+  return 0;
+}
