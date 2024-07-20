df5bc7bcfa77cd2f11a4af39633da6cc45d54829
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-07-20 13:57:04 +0530

    insert node at begin of the singly linked list is completed

---
+++ 30_days_chllanges/day20/_2_insert_node_in_singly_linked_list_at_begin.cpp
@@ -0,0 +1,63 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node(int data)
+  {
+    this->data = data;
+    this->next = nullptr;
+  }
+  static void displayNode(Node *head);
+  static void insertAtHead(Node *&head, int data);
+};
+
+void Node::displayNode(Node *head)
+{
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    cout << temp->data << " ->";
+    temp = temp->next;
+  }
+  cout << "nullptr" << endl;
+}
+
+void Node::insertAtHead(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  newNode->next = head;
+  head = newNode;
+}
+
+// main start here ...
+int main()
+{
+  // create the empty head node pointer
+  Node *head = nullptr;
+  int size;
+  cout << "Enter the size of the linked list : " << endl;
+  cin >> size;
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the element of the node: " << i << endl;
+    cin >> n;
+    // we can access the insertAtHead methods directly bec. we  create the function static
+    Node::insertAtHead(head, n);
+  }
+
+  cout << "Display All node:" << endl;
+  Node::displayNode(head);
+
+  return 0;
+}
