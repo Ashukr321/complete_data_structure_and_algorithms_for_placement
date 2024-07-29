2ff4f446f15c67a65672f7f05636d1914349321a
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-07-29 11:46:15 +0530

    insert the node at begin in the singly linked list

---
+++ stl/pairs/intro.cpp
@@ -0,0 +1,13 @@
+//preprocessor
+#include<iostream>
+using namespace std;
+
+
+//main start here ...
+int main(){
+  pair<int,string>p;
+  p = make_pair(1,"ashutos");
+  return 0;
+}
+
+//pairs in the cpp  
