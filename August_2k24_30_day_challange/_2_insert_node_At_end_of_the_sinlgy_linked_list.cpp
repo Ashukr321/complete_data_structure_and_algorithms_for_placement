df83e5707bc3df1550edeaa1be0b233f1354821a
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-07-29 17:58:15 +0530

    insert the node at the end of the singly linked  list

---
+++ August_2k24_30_day_challange/_2_insert_node_At_end_of_the_sinlgy_linked_list.cpp
@@ -0,0 +1,66 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  // constructor
+  Node(int data)
+  {
+    this->data = data;
+    this->next = nullptr;
+  }
+  static void DisplayNode(Node *head);
+  static void insertNodeAtEnd(Node *&head, int data);
+};
+
+void Node::DisplayNode(Node *head)
+{
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    cout << temp->data << " ->";
+    temp = temp->next;
+  }
+  cout << "NULL" << endl;
+}
+
+void Node::insertNodeAtEnd(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  while (temp->next != nullptr)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+}
+
+// main start here ...
+int main()
+{
+
+  Node *head = nullptr;
+  int size;
+  cout << "Enter the size of the linked list :" << endl;
+  cin >> size;
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the node data at index : " << i << endl;
+    cin >> n;
+    Node::insertNodeAtEnd(head, n);
+  }
+  cout << "Display all the node:" << endl;
+  Node::DisplayNode(head);
+
+  return 0;
+}
