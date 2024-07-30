a3609e260424a32cdbf8d90e153b15dbff0df7c5
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-07-30 15:26:05 +0530

    sum fo the all the node of the linked list

---
+++ August_2k24_30_day_challange/_10_print_the_sum_of_node_data_in_linked_list.cpp
@@ -0,0 +1,77 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node(int data)
+  {
+    this->data = data;
+    this->next = nullptr;
+  }
+  static void displayNode(Node *head);
+  static void insertNodeAtEnd(Node *&head, int data);
+  static int sumOfNode(Node *head);
+};
+
+void Node::displayNode(Node *head)
+{
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    cout << temp->data << " ->";
+    temp = temp->next;
+  }
+  cout << "NULL" << endl;
+}
+void Node::insertNodeAtEnd(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  while (temp->next != nullptr)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+}
+
+int Node::sumOfNode(Node *head)
+{
+  int sum = 0;
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    sum += temp->data;
+    temp = temp->next;
+  }
+  return sum ;
+}
+// main start here ...
+int main()
+{
+
+  Node *head = nullptr;
+  int size;
+  cout << "Enter the size of the linked list :" << endl;
+  cin >> size;
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the node data at index:" << i << endl;
+    cin >> n;
+    Node::insertNodeAtEnd(head, n);
+  }
+  cout << "Display all node:" << endl;
+  Node::displayNode(head);
+  cout << "sum of the all the node data:" << endl;
+  cout << Node::sumOfNode(head) << endl;
+  return 0;
+}
