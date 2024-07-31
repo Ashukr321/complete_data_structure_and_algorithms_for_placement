0617129963891e70bcff72a22f25ff315996abd5
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-07-31 07:15:18 +0530

    insert node at the end of the doubly linked list

---
+++ August_2k24_30_day_challange/day2/_2_insert_node_at_End_of_doubly_linked_list.cpp
@@ -0,0 +1,77 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node *prev;
+  // constructor
+  Node(int data)
+  {
+    this->data = data;
+    this->next = nullptr;
+    this->prev = nullptr;
+  };
+
+  static void displayNode(Node *head);
+  static void insertNodeAtEnd(Node *&head, int data);
+};
+
+void Node::displayNode(Node *head)
+{
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    cout << temp->data << " -> ";
+    temp = temp->next;
+  }
+  cout << "NULL" << endl;
+}
+
+void Node::insertNodeAtEnd(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  while (temp->next != nullptr)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+  newNode->prev = temp;
+}
+// main start here ...
+int main()
+{
+  Node *head = nullptr;
+  int size;
+  cout << "Enter the size of the linked list :" << endl;
+  cin >> size;
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the node data at index " << i << endl;
+    cin >> n;
+    Node::insertNodeAtEnd(head, n);
+  }
+  cout << "Display the node:" << endl;
+  Node::displayNode(head);
+
+  // Don't forget to free the memory!
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    Node *next = temp->next;
+    delete temp;
+    temp = next;
+  }
+
+  return 0;
+}
