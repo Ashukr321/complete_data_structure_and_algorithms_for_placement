ac9111e5bf790b00a12784d97f7e9b256968158a
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-03 00:32:21 +0530

    check the number is armstrong number and also print the range of numbers

---
+++ August_2k24_30_day_challange/day5/_1_check_number_is_armstrong.cpp
@@ -0,0 +1,49 @@
+// preprocessor
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+bool isArmStrong(int num)
+{
+  int originalNum = num;
+  int sum = 0;
+  int cDigit = 0;
+  int temp = num;
+  while (temp!= 0)
+  {
+    cDigit++;
+    temp = temp / 10;
+  }
+  while (num!= 0)
+  {
+    int digit = num % 10;
+    sum = sum + pow(digit, cDigit);
+    num = num / 10;
+  }
+  return originalNum == sum;
+}
+
+vector<int> findAllArmstrong(int start, int end)
+{
+  vector<int> armstrongNumbers;
+  for (int num = start; num <= end; num++) // change num to num++ and add <=
+  {
+    if (isArmStrong(num))
+    {
+      armstrongNumbers.push_back(num);
+    }
+  }
+  return armstrongNumbers;
+}
+
+// main start here...
+int main()
+{
+  vector<int> armstrongNumbers = findAllArmstrong(100, 1000);
+  for (auto num : armstrongNumbers)
+  {
+    cout << num << endl;
+  }
+  return 0;
+}
