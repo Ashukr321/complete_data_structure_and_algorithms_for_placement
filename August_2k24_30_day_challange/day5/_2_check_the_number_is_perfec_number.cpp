483323684647ea9c7dd36103fc03a14f65911b55
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-03 11:14:47 +0530

    check the number is even or odd

---
+++ August_2k24_30_day_challange/day5/_2_check_the_number_is_perfec_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// Function to check if a number is perfect
+bool isPerfect(int num) {
+    int sum = 0;
+    for (int i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+    return (sum == num);
+}
+
+int main() {
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+
+    if (isPerfect(num)) {
+        std::cout << num << " is a perfect number." << std::endl;
+    } else {
+        std::cout << num << " is not a perfect number." << std::endl;
+    }
+
+    return 0;
+}
---
+++ August_2k24_30_day_challange/day5/_3_check_the_number_is_even_or_odd.cpp
@@ -0,0 +1,27 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+void checkEvenOrOdd(int num)
+{
+  if (num <= 0)
+  {
+    cout << "Invalid input" << endl;
+  }
+  if (num % 2 == 0)
+  {
+    cout << "even" << endl;
+  }
+  else
+  {
+    cout << "Odd" << endl;
+  }
+}
+// main start here ...
+int main()
+{
+  int n;
+  cout << "Enter the your number :" << endl;
+  cin >> n;
+  checkEvenOrOdd(n);
+  return 0;
+}
