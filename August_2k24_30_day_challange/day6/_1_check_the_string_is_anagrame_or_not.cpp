324c29471242173c2cb4cb5eb3446dc551634ff3
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-04 07:04:30 +0530

    check the string is anagram or not

---
+++ August_2k24_30_day_challange/day6/_1_check_the_string_is_anagrame_or_not.cpp
@@ -0,0 +1,48 @@
+// preprocessor
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool isAgram(string str1, string str2)
+{
+  string filterString1, filterString2;
+  for (char c : str1)
+  {
+    if (isalnum(c))
+    {
+      filterString1 += tolower(c);
+    }
+  }
+  for (char c : str2)
+  {
+    if (isalnum(c))
+    {
+      filterString2 += tolower(c);
+    }
+  }
+  sort(filterString1.begin(), filterString1.end());
+  sort(filterString2.begin(), filterString2.end());
+
+  return filterString1 == filterString2;
+}
+
+// main start here ...
+int main()
+{
+  string str1;
+  cout << "Enter your first string :" << endl;
+  cin >> str1;
+  string str2;
+  cout << "Enter your 2nd string:" << endl;
+  cin >> str2;
+
+  if (isAgram(str1, str2))
+  {
+    cout << "Both strings are anagrams of each other" << endl;
+  }
+  else
+  {
+    cout << "Both strings are not anagrams of each other" << endl;
+  }
+  return 0;
+}
