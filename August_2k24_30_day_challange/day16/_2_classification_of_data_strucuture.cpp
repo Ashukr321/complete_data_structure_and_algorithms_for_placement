1cf6143dcccec1effe52aa471370b46c88af3313
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-16 18:59:48 +0530

    classification of the data structure and algo

---
+++ August_2k24_30_day_challange/day16/_2_classification_of_data_strucuture.cpp
@@ -0,0 +1,60 @@
+// Preprocessor
+#include <iostream>
+using namespace std;
+
+// Main starts here...
+int main()
+{
+  cout << "Classification of Data Structures:" << endl;
+  cout << "--------------------------------" << endl;
+  cout << "Linear Data Structures:" << endl;
+  cout << "-----------------------" << endl;
+  cout << "1. Array" << endl;
+  cout << "2. Linked List" << endl;
+  cout << "   => Singly Linked List" << endl;
+  cout << "   => Doubly Linked List" << endl;
+  cout << "   => Circularly Linked List" << endl;
+  cout << "3. Stack" << endl;
+  cout << "   => Array-Based Stack" << endl;
+  cout << "   => Linked List-Based Stack" << endl;
+  cout << "4. Queue" << endl;
+  cout << "   => Array-Based Queue" << endl;
+  cout << "   => Linked List-Based Queue" << endl;
+  cout << "   => Circular Queue" << endl;
+
+  cout << "Non-Linear Data Structures:" << endl;
+  cout << "-------------------------" << endl;
+  cout << "1. Tree" << endl;
+  cout << "   => Binary Tree" << endl;
+  cout << "   => Binary Search Tree (BST)" << endl;
+  cout << "   => AVL Tree" << endl;
+  cout << "   => Red-Black Tree" << endl;
+  cout << "   => B-Tree" << endl;
+  cout << "   => B+ Tree" << endl;
+  cout << "   => Trie (Prefix Tree)" << endl;
+  cout << "   => Segment Tree" << endl;
+
+  cout << "2. Graph" << endl;
+  cout << "   => Undirected Graph" << endl;
+  cout << "   => Directed Graph (Digraph)" << endl;
+  cout << "   => Weighted Graph" << endl;
+  cout << "   => Unweighted Graph" << endl;
+
+  cout << "  Algorithms :" << endl;
+  cout << " searching " << endl;
+  cout << " 1    linear search" << endl;
+  cout << " 2    binary search" << endl;
+  cout << " sorting " << endl;
+  cout << " 1    bubble sort" << endl;
+  cout << " 2    selection sort" << endl;
+  cout << " 3    insertion sort" << endl;
+  cout << " 4    merge sort" << endl;
+  cout << " 5    quick sort" << endl;
+  cout << "Recursion " << endl;
+  cout << "Dynamic programming " << endl;
+  cout << "Backtracking" << endl;
+  cout << "greedy Algorithms" << endl;
+  cout << "Devide and conqure" << endl;
+
+  return 0;
+}
