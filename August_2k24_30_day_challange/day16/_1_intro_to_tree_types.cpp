7728adbe7d3511dac0bb97f1182d94c997d6b5ea
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-16 18:48:25 +0530

    classification of the tree

---
+++ August_2k24_30_day_challange/day16/_1_intro_to_tree_types.cpp
@@ -0,0 +1,20 @@
+// Preprocessor
+#include <iostream>
+using namespace std;
+
+// Main starts here...
+int main() {
+    cout << "Types of Tree Data Structures:" << endl;
+    cout << "---------------------------" << endl;
+    cout << "1. Binary Tree: The most common tree data structure." << endl;
+    cout << "2. Binary Search Tree" << endl;
+    cout << "   => AVL Tree (Self-Balancing)" << endl;
+    cout << "   => Red-Black Tree (Self-Balancing)" << endl;
+    cout << "3. B-Tree" << endl;
+    cout << "4. B+ Tree" << endl;
+    cout << "5. Segment Tree" << endl;
+    cout << "6. Trie (Prefix Tree)" << endl;
+    cout << "... and many more..." << endl;
+    
+    return 0;
+}
