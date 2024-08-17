2f5c18aa0a0aed939f8bff381a0ebb4209c92295
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-17 14:29:13 +0530

    find the mid node of the linked list

---
+++ August_2k24_30_day_challange/day17/_4_find_the_mid_node_of_linked_list.cpp
@@ -0,0 +1,85 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  // constructor
+  Node(int data)
+  {
+    this->data = data;
+    this->next = nullptr;
+  }
+  static void printNode(Node *head);
+  static void insertNodeAtEnd(Node *&head, int data);
+  static Node *findMidNode(Node *head);
+};
+Node *Node::findMidNode(Node *head)
+{
+  Node *slow = head;
+  Node *fast = head;
+  while (fast != nullptr && fast->next != nullptr)
+  {
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+  return slow;
+}
+void Node::printNode(Node *head)
+{
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    cout << temp->data << " ->";
+    temp = temp->next;
+  }
+}
+void Node::insertNodeAtEnd(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  while (temp->next != nullptr)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+}
+
+// main start here ...
+int main()
+{
+  Node *head = nullptr;
+  int size;
+  cout << "Enter the size of the linked list:" << endl;
+  cin >> size;
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the node data at index:" << i << endl;
+    cin >> n;
+    Node::insertNodeAtEnd(head, n);
+  }
+
+  cout << "display the node:" << endl;
+  Node::printNode(head);
+  cout<<endl;
+  Node *midNode = Node::findMidNode(head);
+  cout << "mid node of the linked list :" << midNode->data << endl;
+  return 0;
+}
+
+// algo
+
+// create
+// insert
+// print
+// find mid node
+// print mid node data
