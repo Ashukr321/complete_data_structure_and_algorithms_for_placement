a0af175fb71cf02dcdc78df17cd3f1650f9af668
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-20 22:31:59 +0530

    check the number is prime number or  not

---
+++ september_2k24_30_day_Challange/day7/_8_check_number_is_prime_number.cpp
@@ -0,0 +1,33 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num)
+{
+  for (int i = 2; i < num; i++)
+  {
+    if (num % i == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+// main start here ...
+int main()
+{
+  int num;
+  cout << "Enter your number :" << endl;
+  cin >> num;
+
+  if (isPrime(num))
+  {
+    cout << "Number is prime" << endl;
+  }
+  else
+  {
+    cout << "Number is not prime" << endl;
+  }
+  return 0;
+}
