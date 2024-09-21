e71b6dbac065537497ba09abc0bc1d9d7f7e8a6c
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-21 08:22:02 +0530

    check the year is leap year or not

---
+++ september_2k24_30_day_Challange/day8/_4_check_leap_year.cpp
@@ -0,0 +1,30 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+bool isLeapYear(int year)
+{
+  if ((year % 4 == 0 || year % 100 == 0) || year % 400 == 0)
+  {
+    return true;
+  }
+  else
+    return false;
+}
+
+// main start here ...
+int main()
+{
+  int year;
+  cout << "Enter your year :" << endl;
+  cin >> year;
+  if (isLeapYear(year))
+  {
+    cout << year << " is a leap year" << endl;
+  }
+  else
+  {
+    cout << year << " is not a leap year" << endl;
+  }
+  return 0;
+}
