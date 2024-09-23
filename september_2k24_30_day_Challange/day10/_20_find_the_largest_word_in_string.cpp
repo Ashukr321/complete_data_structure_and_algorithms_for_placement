46d2226fd4e156cbc78200bff893263134b33a3a
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-24 00:41:03 +0530

    genrate all the  permutation of the string

---
+++ september_2k24_30_day_Challange/day10/_20_find_the_largest_word_in_string.cpp
@@ -0,0 +1,30 @@
+// preprocessor
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+void maxWordInString(string str)
+{
+  int max = 0;
+  string word;
+  string maxLengthWord;
+  istringstream iss(str);
+  while (iss >> word)
+  {
+    if (max < word.length())
+    {
+      maxLengthWord = word;
+    }
+  }
+  cout << "max length word" << maxLengthWord << endl;
+}
+
+// main start here ...
+int main()
+{
+  string str;
+  cout << "Enter your string :" << endl;
+  getline(cin, str);
+  maxWordInString(str);
+  return 0;
+}
