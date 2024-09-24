9548924f0b4a9630b91ba4839fdd434aeaf172e7
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-24 19:24:09 +0530

    check the string is palindrome or not

---
+++ september_2k24_30_day_Challange/day11/_13_check_string_is_palindrome_or_not.cpp
@@ -0,0 +1,26 @@
+//preprocessor
+#include<iostream>
+using namespace std;
+
+
+bool isPalindrome(string str){
+  for(int i = 0; i<str.length();i++){
+    if(str[i]!=str[str.length()-1-i]){
+      return false;
+    }
+  }
+  return true;
+}
+//main start here ...
+int main(){
+  string str;
+  cout<<"Enter your string :"<<endl;
+  cin>>str;
+  if(isPalindrome(str)){
+    cout<<"String is palindrome"<<endl;
+  }else{
+    cout<<"String is not palindrome"<<endl;
+  }
+
+  return 0;
+}
