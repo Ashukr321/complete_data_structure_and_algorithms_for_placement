5042c6892440877793fd8e2f518ceeecb386db66
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-24 14:04:52 +0530

    check two string are the anagrm or not

---
+++ september_2k24_30_day_Challange/day11/_11_find_lcm_of_two_number.cpp
@@ -0,0 +1,30 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+  // base case
+  if (b == 0)
+  {
+    return a;
+  }
+  return gcd(b, a % b);
+}
+int lcm(int a, int b)
+{
+  return a * b / gcd(a, b);
+}
+
+// main start here ...
+int main()
+{
+  int num1, num2;
+  cout << "Enter your first number  num 1 :" << endl;
+  cin >> num1;
+  cout << "Enter your 2nd number num 2:" << endl;
+  cin >> num2;
+  cout << "lcm of the two number :" << lcm(num1, num2) << endl;
+
+  return 0;
+}
