1ce0a5b3e54338bf611c4c6d85269e1d7029df2d
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-25 07:56:04 +0530

    insertion sort is  completed

--- september_2k24_30_day_Challange/day11/_13_check_string_is_palindrome_or_not.cpp
+++ september_2k24_30_day_Challange/day11/_13_check_string_is_palindrome_or_not.cpp
@@ -1,25 +1,31 @@
-//preprocessor
-#include<iostream>
+// preprocessor
+#include <iostream>
 using namespace std;
 
-
-bool isPalindrome(string str){
-  for(int i = 0; i<str.length();i++){
-    if(str[i]!=str[str.length()-1-i]){
+bool isPalindrome(string str)
+{
+  for (int i = 0; i < str.length(); i++)
+  {
+    if (str[i] != str[str.length() - 1 - i])
+    {
       return false;
     }
   }
   return true;
 }
-//main start here ...
-int main(){
+// main start here ...
+int main()
+{
   string str;
-  cout<<"Enter your string :"<<endl;
-  cin>>str;
-  if(isPalindrome(str)){
-    cout<<"String is palindrome"<<endl;
-  }else{
-    cout<<"String is not palindrome"<<endl;
+  cout << "Enter your string :" << endl;
+  cin >> str;
+  if (isPalindrome(str))
+  {
+    cout << "String is palindrome" << endl;
+  }
+  else
+  {
+    cout << "String is not palindrome" << endl;
   }
 
   return 0;
