07a71b18f781993c6ee3626c64a667bb90166ac9
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-27 07:15:54 +0530

    get intersection of the array

--- september_2k24_30_day_Challange/day14/_7_intersection_of_Two_Array.cpp
+++ september_2k24_30_day_Challange/day14/_7_intersection_of_Two_Array.cpp
@@ -3,11 +3,9 @@
 #include <vector>
 using namespace std;
 
-vector<int> intersectionOfArr(int arr1[], int size1, int arr2[], int size2)
+vector<int> getIntersectionArr(int arr1[], int size1, int arr2[], int size2)
 {
- 
   vector<int> result;
-
   for (int i = 0; i < size1; i++)
   {
     for (int j = 0; j < size2; j++)
@@ -19,48 +17,43 @@ vector<int> intersectionOfArr(int arr1[], int size1, int arr2[], int size2)
       }
     }
   }
-
   return result;
 }
-
 // main start here ...
 int main()
 {
   int size1, size2;
-  cout << "Enter the size of array 1: " << endl;
+  cout << "Enter the size1 " << endl;
   cin >> size1;
-  cout << "Enter the size of array 2: " << endl;
+  cout << "Enter the size 2" << endl;
   cin >> size2;
   int arr1[size1];
   int arr2[size2];
 
-  // Enter elements of array 1
-  cout << "Enter the elements of array 1: " << endl;
+  cout << "Enter the element of array 1 " << endl;
   for (int i = 0; i < size1; i++)
   {
     int n;
-    cout << "Enter the element at index: " << i << endl;
+    cout << "Enter element at index:" << i << endl;
     cin >> n;
     arr1[i] = n;
   }
   cout << endl;
-  cout << "Enter the elements of array 2: " << endl;
+  cout << "Enter the element of array 2 " << endl;
   for (int i = 0; i < size2; i++)
   {
     int n;
-    cout << "Enter the element at index: " << i << endl;
+    cout << "Enter element at index:" << i << endl;
     cin >> n;
     arr2[i] = n;
   }
 
-  vector<int> result = intersectionOfArr(arr1, size1, arr2, size2);
+  vector<int> intersectionArr = getIntersectionArr(arr1, size1, arr2, size2);
 
-  cout << "Intersection of two arrays: ";
-  for (int i = 0; i < result.size(); i++)
+  for (auto it = intersectionArr.begin(); it != intersectionArr.end(); it++)
   {
-    cout << result[i] << " ";
+    cout << *it << " ";
   }
-  cout << endl;
 
   return 0;
 }
