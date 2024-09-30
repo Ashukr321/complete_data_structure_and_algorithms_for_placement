e83091deb3cf693988f9111a2014e7343e76060f
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-30 15:41:23 +0530

    quick sort is completed

---
+++ october_30_day_2k24_challange/day1/_22_quick_sort.cpp
@@ -0,0 +1,64 @@
+// preprocessor
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void printArr(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl; // added to move to a new line after printing the array
+}
+
+int partition(int arr[], int low, int high)
+{
+  int pivot = arr[high];
+  int i = low - 1;
+  for (int j = low; j < high; j++)
+  {
+    if (arr[j] < pivot)
+    {
+      i++; // increment i first
+      swap(arr[i], arr[j]); // corrected the swap statement
+    }
+  }
+  swap(arr[i + 1], arr[high]);
+  return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+  if (low < high)
+  {
+    int pivot = partition(arr, low, high);
+    quickSort(arr, low, pivot - 1);
+    quickSort(arr, pivot + 1, high);
+  }
+}
+
+// main start here ...
+int main()
+{
+  int size;
+  cout << "Enter the size of the array :" << endl;
+  cin >> size;
+  int arr[size];
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the element at index:" << i << endl;
+    cin >> n;
+    arr[i] = n;
+  }
+
+  // before sorting:
+  cout << "Before sorting:" << endl;
+  printArr(arr, size);
+  cout << endl;
+  cout << "After sorting:" << endl;
+  quickSort(arr, 0, size - 1);
+  printArr(arr, size);
+  return 0;
+}
