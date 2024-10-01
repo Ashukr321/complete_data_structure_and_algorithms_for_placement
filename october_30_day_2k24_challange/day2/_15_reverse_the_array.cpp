d6eb6240b1fbfff84159fe96fbf40709b0cedd09
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-10-01 12:20:49 +0530

    reverse the array

---
+++ october_30_day_2k24_challange/day2/_15_reverse_the_array.cpp
@@ -0,0 +1,45 @@
+// preprocessor
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+void printArr(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+}
+void reverseArr(int arr[], int size)
+{
+  int i = 0;5
+  1
+  2
+  int j = size - 1;
+  while (i < j)
+  {
+    swap(arr[i],arr[j]);
+    i++ ;
+    j--;
+  }
+}
+// main start here ...
+int main()
+{
+  int size;
+  cout << "Enter the size of the array:" << endl;
+  cin >> size;
+  int arr[size];
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the element at index:" << i << endl;
+    cin >> n;
+    arr[i] = n;
+  }
+
+  cout << "Before reversing :" << endl;
+  reverseArr(arr, size);
+  printArr(arr, size);
+  return 0;
+}
