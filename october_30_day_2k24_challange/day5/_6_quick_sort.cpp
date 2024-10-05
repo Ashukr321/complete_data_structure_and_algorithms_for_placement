ebc9f3365db621a018d30a871d3ee028537de646
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-10-05 07:55:39 +0530

    dnf sort

--- october_30_day_2k24_challange/day5/_6_quick_sort.cpp
+++ october_30_day_2k24_challange/day5/_6_quick_sort.cpp
@@ -1,22 +1,24 @@
 // preprocessor
 #include <iostream>
-#include<algorithm>
+#include <algorithm>
 using namespace std;
 
-
-
-void printArr(int arr[], int size) {
-  for (int i = 0; i < size; i++) {
+void printArr(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
     cout << arr[i] << " ";
   }
-
 }
 
-int partition(int arr[], int low, int high) {
+int partition(int arr[], int low, int high)
+{
   int pivot = arr[high];
   int i = low - 1;
-  for (int j = low; j < high; j++) { 
-    if (arr[j] < pivot) {
+  for (int j = low; j < high; j++)
+  {
+    if (arr[j] < pivot)
+    {
       i++;
       swap(arr[i], arr[j]);
     }
@@ -25,21 +27,25 @@ int partition(int arr[], int low, int high) {
   return i + 1;
 }
 
-void quickSort(int arr[], int low, int high) {
-  if (low < high) {
+void quickSort(int arr[], int low, int high)
+{
+  if (low < high)
+  {
     int pivot = partition(arr, low, high);
     quickSort(arr, low, pivot - 1);
     quickSort(arr, pivot + 1, high);
   }
 }
 
-int main() {
+int main()
+{
   int size;
   cout << "Enter the size of the array:" << endl;
   cin >> size;
   int arr[size];
   cout << "Enter the elements of the array:" << endl;
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < size; i++)
+  {
     int n;
     cout << "Enter the element at index:" << i << endl;
     cin >> n;
