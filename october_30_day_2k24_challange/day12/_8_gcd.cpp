a571104b2c18b1e16f34f50a53785edd3cdff875
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-10-12 20:20:48 +0530

    gcd of the two number

---
+++ october_30_day_2k24_challange/day12/_8_gcd.cpp
@@ -0,0 +1,23 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+  if (b == 0)
+  {
+    return a;
+  }
+  return gcd(b, a % b);
+}
+// main start here ...
+int main()
+{
+  int a, b;
+  cout << "Enter a :" << endl;
+  cin >> a;
+  cout << "Enter b :" << endl;
+  cin >> b;
+  cout << "Gcd of the two number :" << gcd(a, b) << endl;
+  return 0;
+}
