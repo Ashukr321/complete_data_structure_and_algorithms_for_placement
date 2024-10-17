7d5f181d887374c4e5cd6066e87a0d2a03a8f747
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-10-17 19:10:31 +0530

    find the mid node of the linked list

---
+++ october_30_day_2k24_challange/day17/_6_find_the__mid_node_of_linked_list.cpp
@@ -0,0 +1,72 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  // constructor
+  Node(int data)
+  {
+    this->data = data;
+    this->next = nullptr;
+  }
+};
+
+void DisplayNode(Node *head)
+{
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    cout << temp->data << "->";
+    temp = temp->next;
+  }
+  cout << "NULL" << endl;
+}
+
+//
+void insertNodeAtEnd(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  // check corner case
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  while (temp->next != nullptr)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+}
+Node *findMidNode(Node *head)
+{
+  Node *slow = head;
+  Node *fast = head;
+  while (fast != nullptr && fast->next != nullptr)
+  {
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+  return slow;
+}
+// main start here ...
+int main()
+{
+  Node *head = nullptr;
+  insertNodeAtEnd(head, 1);
+  insertNodeAtEnd(head, 2);
+  insertNodeAtEnd(head, 3);
+  insertNodeAtEnd(head, 4);
+  insertNodeAtEnd(head, 5);
+  cout << "Print the linked list :" << endl;
+  DisplayNode(head);
+  Node *midNode = findMidNode(head);
+  cout << "Mid Node of the linked list is :" << midNode->data << endl;
+
+  return 0;
+}
