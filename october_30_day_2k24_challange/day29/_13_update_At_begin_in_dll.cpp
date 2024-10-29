4a3248f531d54c1913092da6097fdcc56d6d39fb
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-10-29 18:51:04 +0530

    update  doubly linked list at given pos

---
+++ october_30_day_2k24_challange/day29/_13_update_At_begin_in_dll.cpp
@@ -0,0 +1,74 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node *prev;
+  // constructor
+  Node(int data)
+  {
+    this->data = data;
+    this->next = nullptr;
+    this->prev = nullptr;
+  }
+};
+
+void displayNode(Node *head)
+{
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    cout << temp->data << " ->";
+    temp = temp->next;
+  }
+  cout << "NULL" << endl;
+}
+
+void insertNodeAtEnd(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  // check corner case
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  while (temp->next != nullptr)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+  newNode->prev = temp;
+}
+
+void updateAtBegin(Node *&head, int data)
+{
+  // check corner case
+  if (head == nullptr)
+  {
+    return;
+  }
+  head->data = data;
+}
+// main start here ...
+int main()
+{
+  Node *head = nullptr;
+  insertNodeAtEnd(head, 1);
+  insertNodeAtEnd(head, 2);
+  insertNodeAtEnd(head, 3);
+  insertNodeAtEnd(head, 4);
+  insertNodeAtEnd(head, 5);
+  displayNode(head);
+  int updatedData;
+  cout << "Enter your updatedData:" << endl;
+  cin >> updatedData;
+  updateAtBegin(head, updatedData);
+  displayNode(head);
+  return 0;
+}
