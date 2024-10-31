ae4ad372ec68e5da91c591600b4daf46d36fae36
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-10-31 14:37:34 +0530

    delete Node AT begin

---
+++ November_30_days_2k24/day1/_11_delete_node_At_begin_in_doubly_linked_list.cpp
@@ -0,0 +1,71 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node *prev;
+  // constructor
+  Node(int data)
+  {
+    this->data = data;
+    this->next = nullptr;
+    this->prev = nullptr;
+  }
+};
+void displayNode(Node *head)
+{
+  Node *temp = head;
+  while (temp != nullptr)
+  {
+    cout << temp->data << "->";
+    temp = temp->next;
+  }
+  cout << "NULL" << endl;
+}
+
+void insertNodeAtEnd(Node *&head, int data)
+{
+  Node *newNode = new Node(data);
+  // check corner case
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  Node *temp = head;
+  while (temp->next != nullptr)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+  newNode->prev = temp;
+}
+void deleteNodeAtBegin(Node *& head){
+  // check corner case
+  if( head==nullptr ){
+    return;
+  }
+  Node * deletedNode = head;
+  Node* temp = head;
+  head = temp->next;
+  delete deletedNode;
+}
+
+// main start here ...
+int main()
+{
+  Node *head = nullptr;
+  insertNodeAtEnd(head, 1);
+  insertNodeAtEnd(head, 2);
+  insertNodeAtEnd(head, 3);
+  insertNodeAtEnd(head, 4);
+  insertNodeAtEnd(head, 5);
+  displayNode(head);
+  deleteNodeAtBegin(head);
+  displayNode(head);
+  return 0;
+}
