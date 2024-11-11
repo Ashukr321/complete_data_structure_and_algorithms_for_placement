c96d66a5aa947d80465aa07883df8bb15858a9b6
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-11-11 08:48:39 +0530

    lcm of the enter your number

---
+++ November_30_days_2k24/day11/_5_lcm.cpp
@@ -0,0 +1,29 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+int hcf(int a, int b)
+{
+  // base case
+  if (b == 0)
+  {
+    return a;
+  }
+  return hcf(b, a % b);
+}
+int lcm(int a, int b)
+{
+  return a * b / hcf(a, b);
+}
+
+// main start here ...
+int main()
+{
+  int a, b;
+  cout << "Enter the element a :" << endl;
+  cin >> a;
+  cout << "Enter the element b :" << endl;
+  cin >> b;
+  cout << "lcm of the enter your number :" << lcm(a, b) << endl;
+  return 0;
+}
