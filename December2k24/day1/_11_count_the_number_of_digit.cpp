28f079c9a28e5f256f6bc2613b6b9e778bf5d10c
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-12-01 17:10:28 +0530

    count the number of digit

---
+++ December2k24/day1/_11_count_the_number_of_digit.cpp
@@ -0,0 +1,21 @@
+//preprocessor
+#include<iostream>
+using namespace std;
+
+
+int countDigit(int num ){
+  int count = 0;
+  while(num!=0){
+    count++;
+    num/=10;
+  }
+  return count;
+}
+//main start here ...
+int main(){
+  int num;
+  cout<<"Enter the number : ";
+  cin>>num;
+  cout<<"Number of digits in the number "<<num<<" is "<<countDigit(num)<<endl;
+  return 0;
+}
