34557b1e68b197f1cb409f3d928a4fa9b77196c8
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-12-07 08:42:14 +0530

    selection sort

---
+++ December2k24/day7/_3_selection_sort.cpp
@@ -0,0 +1,51 @@
+// preprocessor
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void printArr(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+void selectionSort(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    int minIndex = i;
+    for (int j = i + 1; j < size; j++)
+    {
+      if (arr[j] < arr[minIndex])
+      {
+        minIndex = j;
+      }
+    }
+    swap(arr[i], arr[minIndex]);
+  }
+}
+// main start here ...
+int main()
+{
+  int size;
+  cout << "Enter the size of the array :" << endl;
+  cin >> size;
+  int arr[size];
+  cout << "Enter the element of the arrays" << endl;
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "Enter the element at index:" << i << endl;
+    cin >> n;
+    arr[i] = n;
+  }
+  cout << "Before selection sort :" << endl;
+  printArr(arr, size);
+  // selection sort
+  selectionSort(arr, size);
+  cout<<"After selection sort !"<<endl;
+  printArr(arr, size);
+  return 0;
+}
