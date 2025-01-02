d9712bfda60c216e4a4b880c944ffc16006359af
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-01-02 20:17:33 +0530

    hcf of the two number

---
+++ Dsa_Jan_2025/day6/_6_hcf.cpp
@@ -0,0 +1,26 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+int hcf(int a, int b)
+{
+  // base case
+  if (b == 0)
+  {
+    return a;
+  }
+  return hcf(b, a % b);
+}
+
+// main start here ...
+int main()
+{
+  int a, b;
+  cout << "Enter the element a:" << endl;
+  cin >> a;
+  cout << "Enter the element b:" << endl;
+  cin >> b;
+
+  cout << "hcf  of the two number is :" << hcf(a, b) << endl;
+  return 0;
+}
