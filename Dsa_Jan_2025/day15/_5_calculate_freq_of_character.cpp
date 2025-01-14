8947fe2060325a9b71507c7b091b22878ae87d9d
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-01-15 04:55:00 +0530

    calculate the freq of each character

---
+++ Dsa_Jan_2025/day15/_5_calculate_freq_of_character.cpp
@@ -0,0 +1,25 @@
+//preprocessor
+#include<iostream>
+#include<map>
+using namespace std;
+
+
+void calculate_freq(string str){
+  map<char,int>freqMap;
+  for(int i = 0 ; i<str.length();i++){
+    freqMap[str[i]]++;
+  }
+  // for each loop
+  for(auto it : freqMap){
+    cout<<it.first<<"-> "<<it.second<<endl;
+  }
+}
+//main start here ...
+int main(){
+  string str;
+  cout<<"Enter your string "<<endl;
+  cin>>str;
+  calculate_freq(str);
+
+  return 0;
+}
