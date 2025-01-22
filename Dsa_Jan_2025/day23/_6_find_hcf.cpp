06c2c2c5ec760ee221afd414c700fd6f9b336faf
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-01-23 01:50:42 +0530

    find the hcf  of the number

---
+++ Dsa_Jan_2025/day23/_6_find_hcf.cpp
@@ -0,0 +1,24 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+int hcf(int a, int b)
+{
+  // base cae
+  if (b == 0)
+  {
+    return a;
+  }
+  return hcf(b, a % b);
+}
+
+// main start here ...
+int main()
+{
+  int a, b;
+  cout << "Enter the element  a and b " << endl;
+  cin >> a >> b;
+  cout << "hcf of the enter your number " << hcf(a, b) << endl;
+
+  return 0;
+}
