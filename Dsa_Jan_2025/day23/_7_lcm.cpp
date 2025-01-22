ff904de5ccdf94478a482b0ec8854df244860c25
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-01-23 01:55:05 +0530

    lcm of the enter your number

--- Dsa_Jan_2025/day23/_7_lcm.cpp
+++ Dsa_Jan_2025/day23/_7_lcm.cpp
@@ -4,22 +4,24 @@ using namespace std;
 
 int hcf(int a, int b)
 {
-  // base cae
+  // base case
   if (b == 0)
   {
     return a;
   }
-  
   return hcf(b, a % b);
 }
-
+int lcm(int a, int b)
+{
+  return a * b / hcf(a, b);
+}
 
 // main start here ...
 int main()
 {
   int a, b;
-  cout << "Enter the element  a and b " << endl;
+  cout << "Enter the elements" << endl;
   cin >> a >> b;
-  cout << "hcf of the enter your number " << hcf(a, b) << endl;
+  cout << "lcm of the enter your number " << lcm(a, b) << endl;
   return 0;
 }
