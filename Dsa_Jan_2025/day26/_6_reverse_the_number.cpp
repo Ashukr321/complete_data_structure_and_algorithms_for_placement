adc0825dad1ad7fcd642ef45eae2eebaea1db6e4
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-01-26 19:41:19 +0530

    count the number of zero in the array

--- Dsa_Jan_2025/day26/_6_reverse_the_number.cpp
+++ Dsa_Jan_2025/day26/_6_reverse_the_number.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int reverseNum(int num, int &reverse) {
-    // Base case: if num is 0, return the reverse
-    if (num == 0) {
-        return reverse; // This return is not necessary, but can be kept for clarity
-    }
-    
-    // Update reverse and call the function recursively
-    reverse = reverse * 10 + num % 10;
-    return reverseNum(num / 10, reverse);
+int reverseNum(int num, int &reverse)
+{
+  // Base case: if num is 0, return the reverse
+  if (num == 0)
+  {
+    return reverse; // This return is not necessary, but can be kept for clarity
+  }
+
+  // Update reverse and call the function recursively
+  reverse = reverse * 10 + num % 10;
+  return reverseNum(num / 10, reverse);
 }
 
 // Main starts here ...
-int main() {
-    int num;
-    cout << "Enter your digit: ";
-    cin >> num;
-    
-    int reverse = 0; // Initialize reverse to 0
-    reverseNum(num, reverse); // Call the function to reverse the number
-    
-    cout << "Reverse of number: " << reverse << endl;
+int main()
+{
+  int num;
+  cout << "Enter your digit: ";
+  cin >> num;
 
-    return 0;
+  int reverse = 0;          // Initialize reverse to 0
+  reverseNum(num, reverse); // Call the function to reverse the number
+
+  cout << "Reverse of number: " << reverse << endl;
+
+  return 0;
 }
