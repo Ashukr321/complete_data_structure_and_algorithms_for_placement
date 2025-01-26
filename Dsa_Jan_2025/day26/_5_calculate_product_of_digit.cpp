087836eda539b5ae31eec60071b13da565648f0b
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-01-26 18:57:55 +0530

    calculate the product of the  digit

---
+++ Dsa_Jan_2025/day26/_5_calculate_product_of_digit.cpp
@@ -0,0 +1,24 @@
+// preprocessor
+#include <iostream>
+using namespace std;
+
+int productOfDigit(int num)
+{
+  // base case
+  if (num == 0)
+  {
+    return 1;
+  }
+  int digit = num % 10;
+  // recursive call
+  return digit * productOfDigit(num / 10);
+}
+// main start here ...
+int main()
+{
+  int num;
+  cout << "Enter your digit" << endl;
+  cin >> num;
+  cout << "poduct of digit  " << productOfDigit(num) << endl;
+  return 0;
+}
