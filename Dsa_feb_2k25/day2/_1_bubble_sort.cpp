e1d22d39542e55a9ad691d966e6f3689260735f5
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-02-03 10:28:02 +0530

    bubble sort

---
+++ Dsa_feb_2k25/day2/_1_bubble_sort.cpp
@@ -0,0 +1,48 @@
+// preprocessor
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    cout << arr[i] << "";
+  }
+  cout << endl;
+}
+
+void bubbleSort(int arr[], int size)
+{
+  for (int i = 0; i < size - 1; i++)
+  {
+    for (int j = 0; j < size - 1 - i; j++)
+    {
+      if (arr[j] > arr[j + 1])
+      {
+        swap(arr[j], arr[j + 1]);
+      }
+    }
+  }
+}
+// main start here ...
+int main()
+{
+  int size;
+  cout << "Enter the  size of the array :" << endl;
+  cin >> size;
+  int arr[size];
+  for (int i = 0; i < size; i++)
+  {
+    int n;
+    cout << "enter the  element at index" << i << endl;
+    cin >> n;
+    arr[i] = n;
+  }
+  cout << "Before bubble sort " << endl;
+  printArray(arr, size);
+  cout << "After bubbles  sort " << endl;
+  bubbleSort(arr, size);
+  printArray(arr, size);
+  return 0;
+}
