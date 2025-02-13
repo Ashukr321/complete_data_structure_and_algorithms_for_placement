be58d13b7c77ab504b544aaf5480b069a2ecb9c8
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-02-13 23:47:26 +0530

    find the factorial

---
+++ tcs/array/_7_find_factorial.cpp
@@ -0,0 +1,18 @@
+//preprocessor
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+  if(n==0){
+    return 1;
+  }
+  if(n==1){
+    return 1;
+  }
+  return n * factorial(n-1);
+}
+//main start here ...
+int main(){
+  cout<<factorial(5)<<endl;
+  return 0;
+}
